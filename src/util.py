ecce333a05c67240a2fcbd8d21bd3ddf7c8e3767
muskit <15199219+muskit@users.noreply.github.com>
2024-01-25 16:59:41 -0800

    fix subdir handling for working_path

--- src/util.py
+++ src/util.py
@@ -27,7 +27,7 @@ def project_root(dir_path: tuple[str] = tuple(), file: str = None):
 
 def working_path(dir_path: tuple[str] = tuple(), file: str = None):
     """Returns file path relative to the working ephemeral directory."""
-    dir_path = project_root("run", *dir_path)
+    dir_path = project_root(("run", *dir_path))
     Path(dir_path).mkdir(parents=True, exist_ok=True)
 
     if file is not None:
